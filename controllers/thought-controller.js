b9eea0044e311cf3e6fc1b5d52a6c1b17d633b9b
bootcamp vinni <alekhyavinni12@gmail.com>
2023-09-18 19:27:58 -0400

    tought controller js

---
+++ controllers/thought-controller.js
@@ -0,0 +1,33 @@
+const { Thought } = require('../models');
+
+const thoughtController = {
+    // Gets All Thoughts
+    getAllThought(req, res) {
+        Thought.find({})
+        .select('-__v')
+          .sort({ _id: -1 })
+          .then(dbUserData => res.json(dbUserData))
+          .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+          });
+    },
+
+     // Creates a New Thought
+     createThought({ body }, res) {
+        Thought.create(body)
+          .then(dbUserData => res.json(dbUserData))
+          .catch(err => res.json(err));
+    },
+
+    // Get Thought by ID
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.id })
+          .select('-__v')
+          .then(dbUserData => res.json(dbUserData))
+          .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+          });
+    },
+}
